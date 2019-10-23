0e809056e31a0088f25675381705ed2f35769cb8
Erin Fung <erinfungg@gmail.com>
2019-10-23 16:23:59 -0400

    completed binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,34 @@
-'use strict';
+"use strict";
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let startInd = 0;
+	let endInd = array.length - 1;
+
+	const midpoint = function(startInd, endInd) {
+		return Math.floor((endInd - startInd) / 2) + startInd;
+	};
+	let midInd = midpoint(startInd, endInd);
+
+	if (target > array[endInd]) return false;
+	if (target < array[startInd]) return false;
+
+	while (endInd - startInd > 1) {
+		if (
+			target === array[midInd] ||
+			target === array[startInd] ||
+			target === array[endInd]
+		)
+			return true;
+		if (target < array[midInd]) {
+			endInd = midInd;
+			midInd = midpoint(startInd, endInd);
+		} else if (target > array[midInd]) {
+			startInd = midInd;
+			midInd = midpoint(startInd, endInd);
+		}
+	}
+	return false;
 };
 
 /*
@@ -14,4 +40,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
