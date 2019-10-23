05658f78dd62759ad31c205851969ba1d63cf737
Erin Fung <erinfungg@gmail.com>
2019-10-23 19:14:18 -0400

    completed min jumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
 
 // Complete this algo
 const minJumps = arr => {
+	let jumps = 0;
+	let steps = arr[0];
+	let maxReach = arr[0];
 
+	for (let i = 1; i < arr.length - 1; i++) {
+		maxReach = Math.max(maxReach, arr[i] + i);
+		steps--;
+		if (steps === 0) {
+			jumps++;
+			steps = maxReach - i;
+		}
+	}
+	console.log(jumps + 1);
+	return jumps + 1;
 };
 
-module.exports = minJumps
+module.exports = minJumps;
